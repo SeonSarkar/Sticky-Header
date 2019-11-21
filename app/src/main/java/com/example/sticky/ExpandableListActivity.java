d06ca54f3c4fac15e2d59b8e06c5c67c20088353
suman ghimire <suman.ghimire@f1soft.com>
2019-11-21 10:50:50 +0545

    WIP

--- app/src/main/java/com/example/sticky/ExpandableListActivity.java
+++ app/src/main/java/com/example/sticky/ExpandableListActivity.java
@@ -1,14 +1,60 @@
 package com.example.sticky;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.example.sticky.adapter.ExpandableRecyclerAdapter;
+import com.example.sticky.databinding.ActivityExpandableListBinding;
+import com.example.sticky.databinding.RowListChildItemBinding;
+import com.example.sticky.databinding.RowListHeaderItemBinding;
+import com.seon.hornet.adapters.callback.StickyRecyclerCallBack;
+import com.seon.hornet.adapters.itemdecoration.ItemDecoration;
+import com.seon.hornet.adapters.model.StickyItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ExpandableListActivity extends AppCompatActivity {
+
+    ActivityExpandableListBinding mBinding;
+
+    List<MenuItem> listMenuItem = new ArrayList<>();
+    ExpandableRecyclerAdapter mExpandableRecyclerAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_expandable_list);
+
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_expandable_list);
+
+
+        List<StickyItem<String, MenuItem>> items = new ArrayList<>();
+        items.add(new StickyItem<>(listMenuItem, "header 1"));
+        items.add(new StickyItem<>(listMenuItem, "header 2"));
+        items.add(new StickyItem<>(listMenuItem, "header 3"));
+
+        mExpandableRecyclerAdapter = new ExpandableRecyclerAdapter(items, R.layout.row_list_header_item, R.layout.row_list_child_item,
+                new StickyRecyclerCallBack<String, MenuItem, RowListHeaderItemBinding, RowListChildItemBinding>() {
+                    @Override
+                    public void bindHeader(RowListHeaderItemBinding header, String item) {
+                        header.tvHeaderItem.setText(item);
+                    }
+
+                    @Override
+                    public void bindChild(RowListChildItemBinding child, MenuItem item) {
+                        child.tvChildItem.setText(item.getItemName());
+                    }
+                });
+
+        mBinding.rvExpandable.setLayoutManager(new LinearLayoutManager(this));
+        mBinding.rvExpandable.setHasFixedSize(true);
+        mBinding.rvExpandable.addItemDecoration(new ItemDecoration(mBinding.rvExpandable, mExpandableRecyclerAdapter));
+        mBinding.rvExpandable.setAdapter(mExpandableRecyclerAdapter);
+
+
     }
 }
--- app/src/main/java/com/example/sticky/adapter/ExpandableRecyclerAdapter.java
+++ app/src/main/java/com/example/sticky/adapter/ExpandableRecyclerAdapter.java
@@ -1,60 +1,20 @@
 package com.example.sticky.adapter;
 
-import android.view.View;
-import android.view.ViewGroup;
-
-import androidx.annotation.NonNull;
-import androidx.databinding.DataBindingUtil;
-import androidx.recyclerview.widget.RecyclerView;
-
-import com.example.sticky.databinding.RowExpandableChildBinding;
-import com.example.sticky.databinding.RowExpandableHeaderBinding;
-import com.example.sticky.model.Section;
+import com.seon.hornet.adapters.adapter.StickyAdapter;
+import com.seon.hornet.adapters.callback.StickyRecyclerCallBack;
 
 import java.util.List;
 
 /**
  * Created by suman.ghimire.
  */
-public class ExpandableRecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
+public class ExpandableRecyclerAdapter extends StickyAdapter {
 
-    private List<Section> sectionList;
+    /* private List<StickyItem<H, C>> originalItems;*/
 
-    public ExpandableRecyclerAdapter(List<Section> sectionList) {
-        this.sectionList = sectionList;
+    public ExpandableRecyclerAdapter(List listMenuItems, int headerLayout, int childLayout, StickyRecyclerCallBack callbackInterface) {
+        super(listMenuItems, headerLayout, childLayout, callbackInterface);
     }
 
-    @NonNull
-    @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-    return null;
-    }
 
-    @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-
-    }
-
-    @Override
-    public int getItemCount() {
-        return sectionList.size();
-    }
-
-    class ViewHolderHeader extends RecyclerView.ViewHolder {
-        RowExpandableHeaderBinding rowExpandableHeaderBinding;
-
-        ViewHolderHeader(View view) {
-            super(view);
-            rowExpandableHeaderBinding = DataBindingUtil.bind(view);
-        }
-    }
-
-    class ViewHolderChild extends RecyclerView.ViewHolder {
-        RowExpandableChildBinding rowExpandableChildBinding;
-
-        ViewHolderChild(View view) {
-            super(view);
-            rowExpandableChildBinding = DataBindingUtil.bind(view);
-        }
-    }
 }
